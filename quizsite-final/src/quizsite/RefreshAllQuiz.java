bf90206846c87267a90e022da1d8b91645aeae06
Josh Zhu <jzhu87@stanford.edu>
2013-12-05 00:11:56 -0800

    Update the recent quizzes in RefreshAllQuiz

--- quizsite-final/src/quizsite/RefreshAllQuiz.java
+++ quizsite-final/src/quizsite/RefreshAllQuiz.java
@@ -74,6 +74,30 @@ public class RefreshAllQuiz extends HttpServlet {
 				}
     	    });
     		
+    		//recent quizzes
+    		quizInfo = new ArrayList<String[]>();
+            String queryStmt = "select * from quizzes order by quizID desc limit 5";
+            rs = DatabaseUtils.runSelectQuery(stmt,queryStmt);
+ 			while (rs.next()) {
+ 				String[] tempInfo = {rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7),rs.getString(8),rs.getString(9),rs.getString(10)}; 
+ 				quizInfo.add(tempInfo);
+ 			}
+ 			ArrayList<Quiz> recentQuizzes = new ArrayList<Quiz>();
+     		
+     		for (String[] info: quizInfo){
+     			String[] extractTagFields = {"*"};
+     			String[] tagTableNames = {"tags"};
+     			String[] argsTag = {"quizID = \"" + info[0] + "\""};
+     			ArrayList<String> tags = new ArrayList<String>();
+     			ResultSet tagText = DatabaseUtils.runSelectQuery(stmt, DatabaseUtils.constructSelectQuery(extractTagFields, tagTableNames, argsTag));
+     			while (tagText.next()) {
+     				tags.add(tagText.getString(2));
+     			}
+     			Quiz tempQuiz = new Quiz(info[0],info[1],info[2],info[3], tags, info[4], Integer.parseInt(info[5]), Boolean.parseBoolean(info[6]), Boolean.parseBoolean(info[7]), Boolean.parseBoolean(info[8]), Boolean.parseBoolean(info[9]));
+     			recentQuizzes.add(tempQuiz);
+     		}
+     		request.getServletContext().setAttribute("recentQuizzes", recentQuizzes);
+    		
     		request.getServletContext().setAttribute("allQuizzes", allQuizzes);
 			RequestDispatcher dispatch = request.getRequestDispatcher("admin.jsp");
 			dispatch.forward(request, response);
